a218757f853539a6567732e001df2e878b98cdb5
MarcosEveraldoDev <marcosdev5411@gmail.com>
2022-12-08 22:05:28 -0300

    att aula 14

--- aulas/aula14.py
+++ aulas/aula14.py
@@ -3,9 +3,5 @@ b = 'bbbbbbbb'
 c= 1.985
 string = 'a ={}  b  = {} c = {:.1f}'
 formato = string.format(a,b,c)
-nome = "Luiz"
-idade = 23
-formato = '{1} tem {0} anos'
-print(formato.format(nome, idade))
-print('"ola mundo "')
+
 print(formato)
