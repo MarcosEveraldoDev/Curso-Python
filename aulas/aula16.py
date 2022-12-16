c37b00a1c79b30cd07c3b13adebdbda22be5bd4d
MarcosEveraldoDev <marcosdev5411@gmail.com>
2022-12-16 16:57:59 -0300

    aula 16

---
+++ aulas/aula16.py
@@ -0,0 +1,11 @@
+entrada = input('Digite se vc que entrar ou sair do sistema: ')
+
+if entrada == 'entrar':
+  print("voce entrou no sistema")
+elif entrada == 'sair':
+  print('voce saiu do sistema')
+else:
+  print('voce nao fez nada')
+
+  print('-------------------------------------')
+  print('TERMINOU O CODIGO')
