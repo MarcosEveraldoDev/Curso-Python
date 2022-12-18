bd93b4658e1cfaf95a0a383926171b271593370c
MarcosEveraldoDev <marcosdev5411@gmail.com>
2022-12-18 17:01:05 -0300

    aula 18

---
+++ aulas/aula18.py
@@ -0,0 +1,16 @@
+primeiro_valor = input('Digite um valor: ')
+print('')
+segundo_valor = input('Digite outro valor: ')
+
+if primeiro_valor > segundo_valor:
+  print('')
+  print('O primeiro valor:', primeiro_valor, 'é maior que o segundo valor ' , segundo_valor)
+  print('')
+elif primeiro_valor < segundo_valor:
+  print('')
+  print('O segundo valor: ', segundo_valor,' é maior que o primeiro valor ' , primeiro_valor)
+  print('')
+else:
+  print('')
+  print('Os dois valores sao iguais ')
+  print('')
