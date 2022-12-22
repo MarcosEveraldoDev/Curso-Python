1ce948fa781d22bd2a2a5f4d5f948762a6b6662c
MarcosEveraldoDev <marcosdev5411@gmail.com>
2022-12-22 09:52:00 -0300

    aula 24

---
+++ aulas/aula24.py
@@ -0,0 +1,12 @@
+# > esquerda
+# < direita
+# ^ centro 
+# .1f formatação
+
+variavel = 'abc'
+
+print(f"{variavel:-^20}")
+print(f"{variavel:->20}")
+print(f"{variavel:-^20}")
+print(f"{10000.5645674:.2f}")
+print(f"{100.98445:.1f}")
