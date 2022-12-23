f7773c2c8260f680da7e0ddf91f746cf26e1ea41
MarcosEveraldoDev <marcosdev5411@gmail.com>
2022-12-23 20:13:24 -0300

    aula 26

---
+++ aulas/aula26.py
@@ -0,0 +1,23 @@
+nome = input('Digite seu nome : ')
+
+idade = input('Digite sua idade : ')
+
+if nome == '' or idade == ''  :
+  print('voce deixou campos em brancos')
+else:
+  print('Seu nome é : ' , nome)
+  print('Seu nome invertido é ', nome [::-1] )
+  print('Sua idade é de : ' , idade)
+  if ' ' in nome:
+   print('Seu nome contem espacos')
+  else:
+    print('Seu nome nao contem espaços')
+ 
+  print(f'Seu nome tem {len(nome)} letras')
+  print('A primeira letra do seu nome é :', nome[0])
+  print('A ultima letra do seu nome é :', nome[-1])
+
+  
+ 
+
+
