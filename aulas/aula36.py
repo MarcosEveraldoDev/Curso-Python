603ed49da90499efdfd8254317be1b66641774be
MarcosEveraldoDev <marcosdev5411@gmail.com>
2022-12-27 21:39:23 -0300

    aula 36

---
+++ aulas/aula36.py
@@ -0,0 +1,15 @@
+quantidade_linha = 3
+quantidade_coluna = 3
+linha = 1
+
+while linha <= quantidade_linha:
+
+  coluna = 1
+  while coluna <= quantidade_coluna:
+    print (f'{linha =} {coluna =}')
+    coluna += 1
+  linha += 1
+
+
+
+print('Acabou')
