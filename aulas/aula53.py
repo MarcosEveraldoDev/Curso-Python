e55a6952c906eafbd2eca6b033d6a39461f623ab
MarcosEveraldoDev <marcosdev5411@gmail.com>
2023-03-04 16:06:44 -0300

    aula 53

---
+++ aulas/aula53.py
@@ -0,0 +1,20 @@
+def Print (a,b,c):
+  print('bom dia')
+  print('bom dia')
+  print('bom dia')
+  print('bom dia')
+  
+
+
+
+def imprimir(a, b, c):
+    print(a,b,c)
+  
+  
+imprimir(1,2,3)
+imprimir(4,5,6)
+
+
+
+
+  
