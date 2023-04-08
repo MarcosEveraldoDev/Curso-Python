20d78034c22983e84f7ebcf29a25a036dc30c9d2
MarcosEveraldoDev <marcosdev5411@gmail.com>
2023-04-08 15:11:20 -0300

    Create aula62.py

---
+++ aulas/aula62.py
@@ -0,0 +1,12 @@
+pessoa = {
+  'nome' : 'Marcos',
+  'sobrenome' : 'Everaldo'  
+  
+}
+
+print(pessoa['nome'])
+print(pessoa['sobrenome'])
+print('------------------')
+for chave in pessoa:
+  print(chave,pessoa[chave])
+
